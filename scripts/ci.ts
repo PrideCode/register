66ddfc207258dc252bc8b628fff1feb07435ecdd
liquiddevelopmentnet <info@minemix.de>
2021-11-28 00:45:54 +0100

    [CI] Completed ci

--- scripts/ci.ts
+++ scripts/ci.ts
@@ -1,3 +1,14 @@
-console.log('This function isn\'t completed yet.')
+import fetch from 'node-fetch'
+import crypto from 'crypto'
 
-console.log('aa'+process.env.TOKEN?.substr(3)+'aa')
+const token: string = process.env.TOKEN
+const ip: string = process.env.IP
+
+var shasum = crypto.createHash('sha1')
+
+if(token == undefined || ip == undefined) throw new Error('Environment Variables aren\'t correctly set. Please open issue.')
+
+fetch(`http://${ip}:3021/webhook/push/?token=${token}&uid=${shasum.digest('hex')}`).then(async () => {
+    console.log('Successfully requested deploy: Deploy('+shasum.digest('hex')+') on remote server.')
+})
+
